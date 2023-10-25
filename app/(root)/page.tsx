6ac83b7b0aba703775596568950ff57e9335558b
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2023-10-25 16:39:51 -0300

    usable modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,24 @@
 'use client';
 
+import { useEffect } from "react";
 
-
-import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
 
 
 const SetupPage= () => {
+
+    const onOpen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+      if(!isOpen){
+        onOpen();
+      }
+    }, [isOpen, onOpen]);
+
     return (
       <div className="p-4">
-        <Modal title="Test" description="Test" isOpen onClose={() => {}}>
-            Children
-        </Modal>
+        Store modal
       </div>
     )
   }
